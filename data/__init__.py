9eee82fa9f89b077383e4f5011cc54ff4d217f1a
michael <zz394@duke.edu>
2026-03-05 11:45:50 -0500

    added data split

--- data/__init__.py
+++ data/__init__.py
@@ -1,13 +1,14 @@
 """Data loading and preprocessing modules."""
 
 from .mat_parser import load_deepsqueak_mat, load_all_mat_files
-from .dataset import USVDataset
+from .dataset import USVDataset, create_data_splits
 from .preprocessing import normalize_features, pad_or_truncate
 
 __all__ = [
     "load_deepsqueak_mat",
     "load_all_mat_files",
     "USVDataset",
+    "create_data_splits",
     "normalize_features",
     "pad_or_truncate",
 ]
